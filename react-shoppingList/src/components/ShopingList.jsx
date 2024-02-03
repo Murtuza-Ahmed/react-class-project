2b0ca5de99a7cd616e83062e5491c5c2bf853ffa
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-03 10:38:34 +0500

    paractice

---
+++ react-shoppingList/src/components/ShopingList.jsx
@@ -0,0 +1,17 @@
+function ShopingList() {
+  return (
+    <div className="app-background">
+      <div className="main-container">
+        <div className="add-item-box">
+          <input
+            type="text"
+            className="add-an-item"
+            placeholder="Add an item..."
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ShopingList;
