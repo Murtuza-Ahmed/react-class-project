45578a6a49bded34de9aa9d199c9b184fcfb2f62
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-05 16:18:44 +0500

    shopping list

--- react-shoppingList/src/components/ShopingList.jsx
+++ react-shoppingList/src/components/ShopingList.jsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
+// FUNCTION SHOPPING-LIST
 function ShopingList() {
+  const [item, setItem] = useState([]);
+  const [newItem, setNewItem] = useState("");
+  console.log(item);
   return (
     <div>
       <div className="main-list">
         <div className="input-filed">
-          <input type="text" placeholder="Add Item..." />
+          <input
+            type="text"
+            placeholder="Add Item..."
+            value={item}
+            onChange={(item) => setItem(item.target.value)}
+          />
           <button>ADD</button>
         </div>
-        <ul>
-          <li>
-            <p>task</p>
-          </li>
-          <li>task</li>
-          <li>task</li>
-        </ul>
+        <ul></ul>
       </div>
     </div>
   );
