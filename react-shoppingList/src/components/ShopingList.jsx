c31fef331e8a08ee244c0581243a34d5f92b359e
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-07 16:08:09 +0500

    paractice

--- react-shoppingList/src/components/ShopingList.jsx
+++ react-shoppingList/src/components/ShopingList.jsx
@@ -9,12 +9,31 @@ import {
 } from "react-icons/fa";
 //  FUNCTION SHOPPING-LIST
 function ShopingList() {
+  // HINT: Each "item" in our list names a name,
+  // A Boolean to tell if its been completed, and a quantity
   const [item, setItem] = useState([
     { itemName: "item 1", quantity: 1, isSelected: false },
-    { itemName: "item 2", quantity: 2, isSelected: false },
+    { itemName: "item 2", quantity: 2, isSelected: true },
     { itemName: "item 3", quantity: 3, isSelected: false },
   ]);
-  console.log(item)
+  const [inputValue, setInputValue] = useState("");
+  // HANDLE ADD BUTTON PLUS ICON
+  function handleAddButtonClick() {
+    const newItem = {
+      itemName: inputValue,
+      quantity: 1,
+      isSelected: false,
+    };
+    const newItems = [...item, newItem];
+    setItem(newItems);
+    setInputValue("");
+  }
+  // HANDLE QUANTITY INCREASE BUTTON RIGHT ICON
+  function handleQuantityIncrease(index) {
+    const newItems = [...item];
+    newItems[index].quantity++;
+    setItem(newItems);
+  }
   return (
     <div className="app-background">
       <div className="main-container">
@@ -23,31 +42,43 @@ function ShopingList() {
             type="text"
             className="add-item-input"
             placeholder="Add Item..."
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
           />
-          <FaPlus />
+          <FaPlus onClick={() => handleAddButtonClick()} />
         </div>
         <div className="item-list">
-          <div className="item-container">
-            <div className="item-name">
-              <>
-                <FaCheckCircle />
-                <span className="compeleted"></span>
-              </>
-              <>
-                <FaCircle />
-                <span></span>
-              </>
+          {/* MAP FUNCTION */}
+          {item.map((item, index) => (
+            <div className="item-container">
+              <div className="item-name">
+                {/* HINT: replace false with a boolen
+                indicating the item has been completed or not */}
+                {item.isSelected ? (
+                  <>
+                    <FaCheckCircle />
+                    <span className="compeleted">{item.itemName}</span>
+                  </>
+                ) : (
+                  <>
+                    <FaCircle />
+                    <span>{item.itemName}</span>
+                  </>
+                )}
+              </div>
+              <div className="quantity">
+                <button>
+                  <FaChevronLeft />
+                </button>
+                <span>{item.quantity}</span>
+                <button>
+                  <FaChevronRight
+                    onClick={() => handleQuantityIncrease(index)}
+                  />
+                </button>
+              </div>
             </div>
-            <div className="quantity">
-              <button>
-                <FaChevronLeft />
-              </button>
-              <span>0</span>
-              <button>
-                <FaChevronRight />
-              </button>
-            </div>
-          </div>
+          ))}
         </div>
         <div className="total">Total: </div>
       </div>
