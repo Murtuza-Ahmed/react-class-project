8f79aabda84a50f143821bdf086b1b60113574f1
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-09 18:26:48 +0500

    paractice

--- react-color-change/src/components/App.jsx
+++ react-color-change/src/components/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 function App() {
-    return (
-        <div>App</div>
-    )
+  const [UIColor, setUIColor] = useState(null);
+  return (
+    <div className="App">
+      <div className="App-color-container"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ react-color-change/src/components/CallBack.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+function CallBack() {
+    return (
+        <div>CallBack</div>
+    )
+}
+
+export default CallBack
