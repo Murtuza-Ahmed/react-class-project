17a0c093a06ee8e49b30be31383af47ea0eaf555
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-14 11:46:56 +0500

    mission complete

--- react-wall-of-wander-MUI/src/components/App.jsx
+++ react-wall-of-wander-MUI/src/components/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import Banner from "./Benner";
+import Mission from "./Mission";
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      <Mission />
     </>
   );
 }
---
+++ react-wall-of-wander-MUI/src/components/Mission.jsx
@@ -0,0 +1,23 @@
+import logo2 from "../assets/image/logo2.png";
+function Mission() {
+  return (
+    <>
+      <section id="mission">
+        <h2>Our Mission</h2>
+        <div className="mission-text">
+          <p>
+            <img src={logo2} alt="LOGO wall of Wander" className="missioning" />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cum
+            animi ut obcaecati labore deserunt possimus minima quasi nemo ipsa
+            error tempora eum dicta quia, natus excepturi, repellendus illum
+            quam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eaque, corrupti distinctio, vel alias ullam laboriosam eius nisi,
+            ipsa nesciunt earum debitis labore reprehenderit sequi sapiente
+            officia. Corporis voluptatum quisquam cumque.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+export default Mission;
