b564ef7d7a51b3007c05c084e31196379a366386
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-14 11:59:26 +0500

    popular print section complete

--- react-wall-of-wander-MUI/src/components/App.jsx
+++ react-wall-of-wander-MUI/src/components/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Header";
 import Banner from "./Benner";
 import Mission from "./Mission";
+import PopularPrint from "./PopularPrint";
 function App() {
   return (
     <>
       <Header />
       <Banner />
       <Mission />
+      <PopularPrint />
     </>
   );
 }
---
+++ react-wall-of-wander-MUI/src/components/PopularPrint.jsx
@@ -0,0 +1,31 @@
+import forkit from "../assets/image/fork-it-up.jpg";
+function PopularPrint() {
+  return (
+    <>
+      <div className="stripe">
+        <section id="popular-prints">
+          <h2>Popular Prints</h2>
+          <div className="ul">
+            <div className="li">
+              <figure className="color-red">
+                <img src={forkit} alt="Fork-It-Up" />
+                <figcaption>
+                  <a
+                    href="https://www.google.com/search?q=fork+it+up+pic&sca_esv=561856720&tbm=isch&sxsrf
+                                =AB5stBjV3_ihi4yEg6hFIbz5nZ_TGSf5aA:1693551902853&source=lnms&sa=X&ved=2ahUKEwi0ms687IiBAxV9VaQEHbYPBc8Q_
+                                AUoAXoECAYQAw&biw=1280&bih=683&dpr=1"
+                    target="_blank"
+                  >
+                    Fork It Up
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default PopularPrint;
