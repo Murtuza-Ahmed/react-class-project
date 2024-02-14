4d7d2481da28be2e7fb499384996424032f41be4
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-15 00:52:00 +0500

    problem solve

--- react-wall-of-wander-MUI/src/components/PopularPrintChild.jsx
+++ react-wall-of-wander-MUI/src/components/PopularPrintChild.jsx
@@ -2,26 +2,20 @@ import eyeing from "../assets/image/eyeing-you.jpg";
 function PopularPrintChild() {
   return (
     <>
-      <div className="stripe">
-        <section id="popular-prints">
-          <div className="ul">
-            <div className="li">
-              <figure className="color-orange">
-                <img src={eyeing} alt="Eyeing-You" />
-                <figcaption>
-                  <a
-                    href="https://www.google.com/search?q=eyeing+you+old+car+pic&sca_esv=561856720&tbm=isch&sxsrf=AB5stBjAOiEGZsj2N8t_
+      <div className="li">
+        <figure className="color-orange">
+          <img src={eyeing} alt="Eyeing-You" />
+          <figcaption>
+            <a
+              href="https://www.google.com/search?q=eyeing+you+old+car+pic&sca_esv=561856720&tbm=isch&sxsrf=AB5stBjAOiEGZsj2N8t_
                                     oaDk93Y4p2LEtw:1693552036239&source=lnms&sa=X&ved=2ahUKEwjL
                                     75z87IiBAxX4T6QEHTQUBR8Q_AUoAXoECAEQAw&biw=1280&bih=683&dpr=1"
-                    target="_blank"
-                  >
-                    Eyeing You
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-          </div>
-        </section>
+              target="_blank"
+            >
+              Eyeing You
+            </a>
+          </figcaption>
+        </figure>
       </div>
     </>
   );
