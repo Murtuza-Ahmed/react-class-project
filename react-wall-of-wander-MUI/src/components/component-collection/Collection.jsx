7d916dee15b8760ae3371783af0c70533621be7f
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-02-15 10:36:52 +0500

    file change popart component complete

--- react-wall-of-wander-MUI/src/components/component-collection/Collection.jsx
+++ react-wall-of-wander-MUI/src/components/component-collection/Collection.jsx
@@ -1,26 +1,8 @@
-import popArt from "../../assets/image/pop-art.jpg";
+import PopArt from "./PopArt";
 function Collection() {
   return (
     <>
-      <section id="collections">
-        <h2>Collections</h2>
-        <div className="ul">
-          <div className="li">
-            <figure className="color-orange">
-              <img src={popArt} alt="Pop-Art" />
-              <figcaption>
-                <a
-                  href="https://www.google.com/search?q=pop+art+pic&sca_esv=561856720&tbm=isch&sxsrf=AB5stBgFMUhwzJ8Ak7Jrf2sDdqdFdWxS
-                                 _w:1693552410751&source=lnms&sa=X&ved=2ahUKEwifpueu7oiBAxWVdqQEHdpzDFYQ_AUoAXoECAIQAw&biw=1280&bih=683&dpr=1"
-                  target="_blank"
-                >
-                  Pop Art
-                </a>
-              </figcaption>
-            </figure>
-          </div>
-        </div>
-      </section>
+      <PopArt />
     </>
   );
 }
