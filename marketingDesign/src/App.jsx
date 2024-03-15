86e215a8a489aba7bf8c19f5ca9d0efc68afa2c9
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-16 00:47:04 +0500

    project start

--- marketingDesign/src/App.jsx
+++ marketingDesign/src/App.jsx
@@ -1,5 +1,20 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <h2>M.S</h2>
+        <navbar>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Project</li>
+            <li>Contact</li>
+          </ul>
+        </navbar>
+      </header>
+    </div>
+  );
 }
 
 export default App;
