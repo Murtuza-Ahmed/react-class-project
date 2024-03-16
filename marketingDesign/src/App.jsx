11fabe62d7b040804b447bdb049b6c24cbea13c5
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-16 22:59:10 +0500

    paractice

--- marketingDesign/src/App.jsx
+++ marketingDesign/src/App.jsx
@@ -1,18 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div>
-      <header>
-        <h2>M.S</h2>
-        <navbar>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Project</li>
-            <li>Contact</li>
-          </ul>
-        </navbar>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ marketingDesign/src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <div className="logo">
+            <h2>M.S</h2>
+          </div>
+          <div className="ul">
+            <ul></ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+export default Header;
