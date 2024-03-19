06720e20660b0b0c6e53c81d27e3020e52a28cf7
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-19 08:41:53 +0500

    bug

--- age-calculator/src/components/AgeCalculator.jsx
+++ age-calculator/src/components/AgeCalculator.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import "../App.css";
 const AgeCalculator = () => {
   const [age, setAge] = useState("");
+  const [day, setDay] = useState("");
   function CalculateAge() {
     let userDate = new Date(age);
-    let date = new Date();
-    
-    console.log(newDate);
-    console.log(userDate);
-    console.log(date);
+    let currentDate = new Date();
+    let userDay = userDate.getDay();
+    let userMonth = userDate.getMonth();
+    let userYear = userDate.getFullYear();
+    let currentDay = currentDate.getDay();
+    let currentMonth = currentDate.getMonth();
+    let currentYear = currentDate.getFullYear();
+    let setDay = userDay - currentDay;
+    let minusMonth = userMonth - currentMonth;
+    let minusYear = currentYear - userYear;
   }
   return (
     <div>
@@ -20,6 +26,9 @@ const AgeCalculator = () => {
           <input type="date" onChange={(e) => setAge(e.target.value)} />
           <button onClick={CalculateAge}>Calculate</button>
         </div>
+        <div>Year{minusYear}</div>
+        <div>Month{minusMonth}</div>
+        <div>Days{minusYear}</div>
       </div>
     </div>
   );
