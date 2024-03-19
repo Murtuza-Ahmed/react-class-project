ee2b0aa117e37056b79c4a0ce61a15daf21aa292
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-20 00:01:09 +0500

    header complete and with out responsive

--- marketingDesign/src/components/Header.jsx
+++ marketingDesign/src/components/Header.jsx
@@ -1,4 +1,35 @@
+import { FaSearch } from "react-icons/fa";
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <header>
+          <nav>
+            <div className="logo">
+              <h1>M.S</h1>
+            </div>
+            <div className="ul">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Project</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            <div className="main">
+              <div className="icon">
+                <FaSearch />
+              </div>
+              <div className="button">
+                <button>Sign In</button>
+              </div>
+            </div>
+          </nav>
+        </header>
+      </div>
+    </div>
+  );
 };
 export default Header;
