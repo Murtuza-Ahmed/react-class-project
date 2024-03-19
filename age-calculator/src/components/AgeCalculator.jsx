28c649bab146979cfbc31b6d3c96d86ce9f2ecbd
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-19 15:42:57 +0500

    complete project

--- age-calculator/src/components/AgeCalculator.jsx
+++ age-calculator/src/components/AgeCalculator.jsx
@@ -1,6 +1,28 @@
 import { useState } from "react";
 import "../App.css";
 const AgeCalculator = () => {
+  const [age, setAge] = useState("");
+  const [minusDay, setMinusDay] = useState(0);
+  const [minusMonth, setMinusMonth] = useState(0);
+  const [minusYear, setMinusYear] = useState(0);
+
+  function CalculateAge() {
+    let userDate = new Date(age);
+    let currentDate = new Date();
+    let userDay = userDate.getDate();
+    let userMonth = userDate.getMonth() + 1;
+    let userYear = userDate.getFullYear();
+    let currentDay = currentDate.getDate();
+    let currentMonth = currentDate.getMonth() + 1;
+    let currentYear = currentDate.getFullYear();
+    let diffDays = currentDay - userDay;
+    let diffMonths = userMonth - currentMonth;
+    let diffYear = currentYear - userYear;
+
+    setMinusDay(diffDays);
+    setMinusMonth(diffMonths);
+    setMinusYear(diffYear);
+  }
   return (
     <div>
       <div className="container">
@@ -11,9 +33,18 @@ const AgeCalculator = () => {
           <input type="date" onChange={(e) => setAge(e.target.value)} />
           <button onClick={CalculateAge}>Calculate</button>
         </div>
-        <div>Year{minusYear}</div>
-        <div>Month{minusMonth}</div>
-        <div>Days{minusYear}</div>
+        <div className="calculator">
+          <div className="day">
+            <span>Days</span> <span className="minus-day">{minusDay}</span>
+          </div>
+          <div className="month">
+            <span>Months</span>{" "}
+            <span className="minus-month">{minusMonth}</span>
+          </div>
+          <div className="year">
+            <span>Years</span> <span className="minus-year">{minusYear}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
