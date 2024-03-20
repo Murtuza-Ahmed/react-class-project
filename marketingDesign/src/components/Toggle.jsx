3c7440fa5b6ddde893e8c674979179334f405ef5
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-20 08:12:12 +0500

    function work

--- marketingDesign/src/components/Toggle.jsx
+++ marketingDesign/src/components/Toggle.jsx
@@ -2,13 +2,16 @@ import { FaBars } from "react-icons/fa";
 import { VscChromeClose } from "react-icons/vsc";
 
 const Toggle = () => {
+  function OpenToggle() {
+    console.log("click");
+  }
   return (
     <div>
       <div className="icon-toggle">
-        <div className="toogle-open">
-          <FaBars />
+        <div className="toggle-open">
+          <FaBars onClick={OpenToggle} />
         </div>
-        <div className="toogle-close">
+        <div className="toggle-close">
           <VscChromeClose />
         </div>
       </div>
